726e3a09c7a70e460e062e8ebdbbe66973e70d36
Wilson <wilkom2009@yahoo.fr>
2020-05-21 16:09:22 +0000

    Routing, boostraping  on mytodo-app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 //import FirstFunction from './components/examples/FirstFunction';
 import Counter from './components/counters/counter'
+import TodoApp from './components/todo/TodoApp';
 import './App.css';
+import './bootstrap.css';
+
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      {/*<Counter/>*/}
+      <TodoApp/>
     </div>
   );
 }
